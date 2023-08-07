aa6b87d0b82e1f1f9034bf16034d2247559c909c
martintokman <info@tokman.com.ar>
2023-08-07 11:12:54 -0300

    Termino diagramas de flujo y agrego funcionalidad play again cuando el juego termina.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from roll_dice import roll_dice
 import sys, os
 
 os.system("clear")
+
 while True:
     
     try:
@@ -10,19 +11,29 @@ while True:
         global_scores = [0 for i in range(number_of_players)]
         
         if number_of_players > 1:
+            
             while True:
+                
                 for count, value in enumerate(global_scores):
+                    
                     print(f"\n\nPlayer #{count+1}, is your turn to play.")
                     global_score = roll_dice(global_scores, count)
+                    
                     if global_score == 99:
                         global_scores[count] = 0
+                    
                     else:
-                        if global_score == 1111:
-                            print(f"Player #{count+1} wins!!!")
-                            sys.exit()
+                        
+                        if global_score == 1111: #si hay un ganador salgo del bucle for
+                            break
+
                         else:        
                             global_scores[count] = global_score
-
+  
+                
+                if global_score == 1111: #si hay un ganador salgo del bucle while
+                    break
+                
                 
                 print("\n\nRound finished. Overall scores are the following.")
 
@@ -30,18 +41,47 @@ while True:
                     print(f"Player #{count+1} has {value}.")
 
                 while True:
+                    
                     _continue = input("\nA new round will start, press (y) to continue, (q) to end game: ")
+                    
                     if _continue.capitalize() != "Y" and _continue.capitalize() != "Q":
                         print("Invalid option, try again.")
                         continue
+                    
                     elif _continue.capitalize() == "Q":
                         print("Thank you for playing. Goodbye :)")
                         sys.exit()
+                    
                     else:
                         break
                 
                 os.system("clear")
-        
+
+            
+            if global_score == 1111: #si hay un ganador termina el juego
+                    
+                    print(f"Player #{count+1} wins!!!\n")
+                    
+                    while True:
+                        
+                        play_again = input("Play again? (y/n): ")
+                        play_again = play_again.capitalize()
+                        
+                        if play_again != "Y" and play_again != "N":
+                            print("Option not allowed, try again.")
+                            continue
+                        
+                        else:
+                            break
+                    
+                    if play_again == "Y":
+                        os.system("clear")
+                        continue
+                    
+                    elif play_again == "N":
+                        sys.exit()
+            
+
         else:
             print("The minimum number of players is two, try again.\n")
     
@@ -50,7 +90,11 @@ while True:
         print("Only numbers allowed, try again\n")
         continue
 
-
     
+
+
+
+
+
 
         
--- roll_dice.py
+++ roll_dice.py
@@ -1,18 +1,30 @@
 import random, os
 
-
-
-
 def roll_dice(global_scores, count):
+    """Recieves the list of global scores and the number of the current player.
+       Allow the player to roll dice until a winner is set, dice is 0 or 
+       the player doesn't want to roll again. 
+
+    Args:
+        global_scores (list): Current list of global scores for all players
+        count (_type_): Player counter
+
+    Returns:
+        global_scores: Updated list of global scores for all players
+    """
+    
     global_score = global_scores[count]
-    threshold = 20
+    threshold = 10
+    
     while True:
         
         while True:
             roll_dice = input("Roll dice? (y/n): ")
             roll_dice = roll_dice.capitalize()
+            
             if roll_dice != "Y" and roll_dice !="N":
                  print("Invalid option, try again.")
+            
             else:
                  break
         
@@ -22,11 +34,14 @@ def roll_dice(global_scores, count):
             print(f"Dice was: {dice}")
             
             if dice > 1:
+                
                 global_score += dice
                 print(f"Your global score is: {global_score}\n")
+                
                 if global_score >= threshold:
                     global_score = 1111 #winner code
                     break
+                
                 else:
                     continue
         
@@ -35,10 +50,9 @@ def roll_dice(global_scores, count):
             
             
         if dice == 1:
-                print("Your global score is 0")
-                global_score = 99
-                
-                break
+            print("Your global score is 0")
+            global_score = 99
+            break
         
     return global_score
 
